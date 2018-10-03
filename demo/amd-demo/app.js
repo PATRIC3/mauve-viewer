6ad0687a882bb0f243a5892c043a8bfa7de4dabf
nconrad <nconrad@anl.gov>
2018-10-03 15:34:35 -0500

    move demo data

--- demo/amd-demo/app.js
+++ demo/amd-demo/app.js
@@ -16,7 +16,7 @@ function(MauveViewer, d3, axios) {
 
     let ele = document.getElementById('chart');
 
-    axios.get(`../brucella-lcbs.json`).then(res => {
+    axios.get(`../data/brucella-lcbs.json`).then(res => {
         let data = res.data;
         let chart = new MauveViewer.default({data, d3, ele})
     }).catch(error => 'An error occurred while loading the component');
