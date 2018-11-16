ac0437ff1db4c3d963daed274eb9835dd5fcb662
nconrad <nconrad@anl.gov>
2018-11-16 14:27:45 -0600

    add simple loading status

--- demo/main.js
+++ demo/main.js
@@ -5,27 +5,49 @@ import * as d3 from 'd3';
 import axios from 'axios';
 
 
-// get alignment
-axios.get(`data/more-brucella.json`).then(res => {
-    let data = res.data;
+document.addEventListener('DOMContentLoaded', () => {
     let ele = document.getElementById('chart');
+    let statusHandle = loading(ele);
 
-    // use organism labels for each track (optional)
-    let ext;
-    let ids = [];
-    data.forEach(lcbs => {
-        lcbs.forEach(r => {
-            ext = r.name.split('.').pop();
-            let name = r.name.replace(`.${ext}`, '');
-            if (!ids.includes(name)) ids.push(name);
-        });
-    });
+    // get alignment
+    axios.get(`data/3-ecoli.json`).then(res => {
+        let data = res.data;
 
-    getMauveData({genomeIDs: ids, ext})
-        .then(({labels, contigs, features}) => {
-            new MauveViewer({
-                ele, data, d3,
-                labels, features, contigs
+        // use organism labels for each track (optional)
+        let ext;
+        let ids = [];
+        data.forEach(lcbs => {
+            lcbs.forEach(r => {
+                ext = r.name.split('.').pop();
+                let name = r.name.replace(`.${ext}`, '');
+                if (!ids.includes(name)) ids.push(name);
             });
         });
+
+
+        getMauveData({genomeIDs: ids, ext})
+            .then(({labels, contigs, features}) => {
+                clearInterval(statusHandle);
+                new MauveViewer({
+                    ele, data, d3,
+                    labels, features, contigs
+                });
+            }).catch(e => {
+                clearInterval(statusHandle);
+                ele.innerHTML = `<br>Something has gone wrong. ` +
+                    `Please feel free to report this.<br><br>` +
+                    `Error: ${e.message}`;
+            });
+    });
 });
+
+
+function loading(ele) {
+    let i = 0;
+    let handle = setInterval(() => {
+        ele.innerHTML = `<br>loading${'.'.repeat(i % 4)}`;
+        i += 1;
+    }, 300);
+
+    return handle;
+}
